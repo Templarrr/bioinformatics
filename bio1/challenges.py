49b2b9260baaee15f05776330790c2660f2a5aaf
Michael Odintsov <michael@datarobot.com>
2016-05-29 15:28:00 +0300

    FrequentWords challenge

--- bio1/challenges.py
+++ bio1/challenges.py
@@ -1,7 +1,14 @@
-from common import count_kmers
+from common import count_kmers, frequent_words
 
 
 def kmer_challenge():
     with open('dataset_2_6.txt', 'r') as f:
         lines = f.read().splitlines()
     print count_kmers(lines[0], lines[1])
+
+
+def frequent_words_challenge():
+    with open('dataset_2_9.txt', 'r') as f:
+        lines = f.read().splitlines()
+    words = frequent_words(lines[0], int(lines[1]))
+    print ' '.join(words)
