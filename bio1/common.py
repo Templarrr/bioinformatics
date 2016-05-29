74b9628b9d4296d2f443a3f1084581ad4f6c8bd6
Michael Odintsov <michael@datarobot.com>
2016-05-29 18:26:18 +0300

    add reversed keyword in complementary string

--- bio1/common.py
+++ bio1/common.py
@@ -1,13 +1,15 @@
 import constants
 
 
-def get_complementary_string(nucleotides_string, rna_mode=False):
+def get_complementary_string(nucleotides_string, rna_mode=False, reversed=False):
     complements = constants.rna_complementary \
         if rna_mode \
         else constants.dna_complementary
     result = ''
     for nucleotide in nucleotides_string:
         result += complements[nucleotide]
+    if reversed:
+        result = result[::-1]
     return result
 
 
--- bio1/test.py
+++ bio1/test.py
@@ -5,6 +5,8 @@ def test_complementary_string():
     input_string = 'ACGCGA'
     assert get_complementary_string(input_string, rna_mode=False) == 'TGCGCT'
     assert get_complementary_string(input_string, rna_mode=True) == 'UGCGCU'
+    assert get_complementary_string(input_string, rna_mode=True, reversed=True) == 'UCGCGU'
+    assert get_complementary_string(input_string, rna_mode=False, reversed=True) == 'TCGCGT'
 
 
 def test_count_kmers():
