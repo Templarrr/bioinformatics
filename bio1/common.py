66b0c42e0f9c0d7b5220b8c2f173eefcb6c8e4f7
Michael Odintsov <michael@datarobot.com>
2016-05-29 14:35:46 +0300

    count_kmers

--- bio1/common.py
+++ bio1/common.py
@@ -9,3 +9,13 @@ def get_complementary_string(nucleotides_string, rna_mode=False):
     for nucleotide in nucleotides_string:
         result += complements[nucleotide]
     return result
+
+
+def count_kmers(nucleotides_string, kmer):
+    kmer_len = len(kmer)
+    nucleotides_string_len = len(nucleotides_string)
+    count = 0
+    for i in range(0, nucleotides_string_len - kmer_len + 1):
+        if nucleotides_string[i:i + kmer_len] == kmer:
+            count += 1
+    return count
--- bio1/test.py
+++ bio1/test.py
@@ -1,7 +1,12 @@
-from common import get_complementary_string
+from common import get_complementary_string, count_kmers
 
 
 def test_complementary_string():
     input_string = 'ACGCGA'
     assert get_complementary_string(input_string, rna_mode=False) == 'TGCGCT'
     assert get_complementary_string(input_string, rna_mode=True) == 'UGCGCU'
+
+
+def test_count_kmers():
+    assert count_kmers('CGATATATCCATAG', 'ATA') == 3
+    assert count_kmers('CGATATATCCATAGATA', 'ATA') == 4
