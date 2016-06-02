33f2515c44b6229255c96136c763e98a5140da3d
Michael Odintsov <michael@datarobot.com>
2016-06-02 19:01:59 +0300

    hemming distance challenge

--- bio2/challenges.py
+++ bio2/challenges.py
@@ -1,6 +1,6 @@
 from time import time
 
-from common import min_skew
+from common import min_skew, hemming_distance
 
 
 def min_skew_challenge():
@@ -10,10 +10,17 @@ def min_skew_challenge():
     print ' '.join(map(str, min_skew_pos))
 
 
+def hemming_distance_challenge():
+    with open('../data/challenges/dataset_9_3.txt', 'r') as f:
+        lines = f.read().splitlines()
+    print hemming_distance(lines[0], lines[1])
+
+
 def run_all():
     print 'Running all challenges with timing'
     time1 = time()
     min_skew_challenge()
+    hemming_distance_challenge()
     time2 = time()
     print 'Comlete in %f seconds' % (time2 - time1)
 
