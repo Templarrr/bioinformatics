f51726a57ddcae7e9aadaeca47f4d04d921bf67d
Michael Odintsov <michael@datarobot.com>
2016-06-08 19:51:33 +0300

    subtle_motif_enumeration_challenge

--- bio3/challenges.py
+++ bio3/challenges.py
@@ -10,5 +10,14 @@ def motif_enumeration_challenge():
     print ' '.join(motifs)
 
 
+def subtle_motif_enumeration_challenge():
+    with open('../data/challenges/15_4_implanted_motif_2.txt', 'r') as f:
+        lines = f.read().splitlines()
+    motifs = motif_enumeration(lines,
+                               15,
+                               4)
+    print ' '.join(motifs)
+
+
 if __name__ == '__main__':
     motif_enumeration_challenge()
